9ff943585da52e2fba6cd81b04cb0c7094f8d6e1
Mehedi <maaztech@gmail.com>
2025-08-11 23:13:40 +0600

    json data created

--- src/components/Accesories/Accesories.jsx
+++ src/components/Accesories/Accesories.jsx
@@ -1,8 +1,21 @@
+import { useLoaderData } from "react-router";
 
 const Accesories = () => {
+     const allItems = useLoaderData();
+
     return (
         <div>
-            
+            <h1 className="text-5xl text-center font-bold">Explore Cutting-Edge Gadgets</h1>
+            <div>
+                <div className="categories">
+                    {
+                        allItems.map(cat => <button key={cat.product_id}>{cat.category}</button>)
+                    }
+                </div>
+                <div className="categories-items">
+
+                </div>
+            </div>
         </div>
     );
 };
